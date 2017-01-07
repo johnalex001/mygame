01534f66cf175bb66fef9381c7b5bf6058d7de68
burningDown <734556742@qq.com>
2017-01-07 14:59:19 +0800

    second

--- hello.c
+++ hello.c
@@ -4,6 +4,7 @@ int main(){
 	cout<<"Hello,yyt~~"<<endl;
 	cout<<"Hello,hanxu~~~~~~~~~~~~~~~~~~~"<<endl; 
 	cout<<"hhhhh"<<endl;
+	cout<<"Hello,JHF"<<endl;
        ///Ìí¼Ócout<<"Hello,XXX"<<endl;/// 
 	return 0;
 } 
